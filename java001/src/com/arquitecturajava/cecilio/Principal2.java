3495d42f9a1736e8964d2c29370930976e9eb030
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-22 10:27:39 +0200

    cambio de package

--- java001/src/com/arquitecturajava/cecilio/Principal2.java
+++ java001/src/com/arquitecturajava/cecilio/Principal2.java
@@ -1,4 +1,4 @@
-package com.arquitecturajava;
+package com.arquitecturajava.cecilio;
 
 public class Principal2 {
 
---
+++ java001/src/com/arquitecturajava/cecilio/Principal6.java
@@ -0,0 +1,13 @@
+package com.arquitecturajava.cecilio;
+
+public class Principal6 {
+
+	public static void main(String[] args) {
+
+		for (int j = 0; j < 10; j++) {
+			System.out.println(j);
+		}
+
+	}
+
+}
