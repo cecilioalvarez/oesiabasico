bd4e519e132ef9a10f7a4b1983c4fd0349034a3b
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-22 11:13:41 +0200

    bucle de array

--- java001/src/com/arquitecturajava/cecilio/Principal8.java
+++ java001/src/com/arquitecturajava/cecilio/Principal8.java
@@ -3,7 +3,16 @@ package com.arquitecturajava.cecilio;
 public class Principal8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	int[] lista= new int[] { 6,7,8,2,3,4};
+		
+		
+		for (int i = 0; i < lista.length; i++) {
+			System.out.println(lista[i]);
+
+		}
+
+	
 
 	}
 
