c6367462b38981b24a0c7da2dd30a4fa16332942
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-22 12:33:13 +0200

    switch

--- java001/src/com/arquitecturajava/cecilio/Principal10.java
+++ java001/src/com/arquitecturajava/cecilio/Principal10.java
@@ -3,7 +3,15 @@ package com.arquitecturajava.cecilio;
 public class Principal10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int nota=9 ;
+		
+		
+		if (nota <=1 || nota>=9) {
+			
+			System.out.println("te has salido de la tabla");
+		}
+		
 
 	}
 
---
+++ java001/src/com/arquitecturajava/cecilio/PrincipalSwitch.java
@@ -0,0 +1,27 @@
+package com.arquitecturajava.cecilio;
+
+public class PrincipalSwitch {
+
+	public static void main(String[] args) {
+
+		int categoria = 1;
+
+		switch (categoria) {
+
+		case 1:
+			System.out.println("categoria 1");
+			break;
+		case 2:
+			System.out.println("categoria 2");
+			break;
+		case 3:
+			System.out.println("categoria 3");
+			break;
+
+		default:
+			System.out.println("no hacemos nada");
+		}
+
+	}
+
+}
