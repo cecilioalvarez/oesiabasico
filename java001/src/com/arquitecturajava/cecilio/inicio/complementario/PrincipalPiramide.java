42f45eabfdc0c29c7e522190a97df411e791720e
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-22 13:01:11 +0200

    bucles anidados

---
+++ java001/src/com/arquitecturajava/cecilio/inicio/complementario/PrincipalPiramide.java
@@ -0,0 +1,17 @@
+package com.arquitecturajava.cecilio.inicio.complementario;
+
+public class PrincipalPiramide {
+
+	public static void main(String[] args) {
+		
+		for (int i=1;i<=5;i++) {
+			
+			for (int j = 1; j <=i; j++) {
+				
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+	}
+}
