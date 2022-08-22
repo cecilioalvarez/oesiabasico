b1dde78acc42ee8299d41bf9023f40312bc856b4
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-22 11:45:41 +0200

    bucle mayor

--- java001/src/com/arquitecturajava/cecilio/Principal8.java
+++ java001/src/com/arquitecturajava/cecilio/Principal8.java
@@ -18,6 +18,26 @@ public class Principal8 {
 		}
 
 		System.out.println("la suma es "+total);
+	
+		
+		for (int i = lista.length-1; i >=0; i--) {
+			System.out.println(lista[i]);
+
+		}
+		
+		int mayor=0;
+		
+		for (int i = lista.length-1; i >=0; i--) {
+			
+			if (lista[i]>mayor) {
+				mayor=lista[i];
+			}
+
+		}
+		System.out.println("************");
+		System.out.println(mayor);
+	
+	
 	}
 
 }
