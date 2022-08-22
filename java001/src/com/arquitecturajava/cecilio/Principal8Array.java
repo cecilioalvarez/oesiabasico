8eb48167c965b5702dedb012af31568830ec4e69
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-22 10:58:04 +0200

    cambios felix

---
+++ java001/src/com/arquitecturajava/cecilio/Principal8Array.java
@@ -0,0 +1,16 @@
+package com.arquitecturajava.cecilio;
+
+public class Principal8Array {
+
+	public static void main(String[] args) {
+		
+		int[] lista= new int[] { 6,7,8,2,3,4};
+		
+		System.out.println("hola felix");
+		for (int i = 0; i < lista.length; i++) {
+			System.out.println(lista[i]);
+		}
+
+	}
+
+}
