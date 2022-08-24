8cdb3bdbc5bae9064af89046c3d410e04bf111b7
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-24 08:19:21 +0200

    igualdad

---
+++ java001/src/com/arquitecturajava/oo5/Principal.java
@@ -0,0 +1,13 @@
+package com.arquitecturajava.oo5;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Persona p= new Persona("1","antonio");
+		Persona p2= new Persona("2","miguel");
+		System.out.println(p.equals(p2));
+
+	}
+
+}
