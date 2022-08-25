6a7b7b2bc65a5476b18f00b1fc861afc490c9329
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-25 09:16:16 +0200

    primera revision excepcion

--- java001/src/com/arquitecturajava/excepciones/Principal3.java
+++ java001/src/com/arquitecturajava/excepciones/Principal3.java
@@ -3,49 +3,43 @@ package com.arquitecturajava.excepciones;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 public class Principal3 {
 
 	public static void main(String[] args) {
-		
-		
+
 		JFrame frame = new JFrame("Hello World Java Swing");
-
-        // set frame site
-        frame.setMinimumSize(new Dimension(800, 600));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // center the JLabel
-        JLabel lblText = new JLabel("Hello World!", SwingConstants.CENTER);
-
-        JButton button = new JButton("pulsame");
-        button.setBounds(130,200,100,40);//Setting location and size of button
-
-        frame.getContentPane().setLayout(null);
-        
-        button.addActionListener(new ActionListener() {
-			
+		frame.setMinimumSize(new Dimension(800, 600));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton button = new JButton("pulsame");
+		button.setBounds(130, 200, 100, 40);
+		frame.getContentPane().setLayout(null);
+		button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+
+				File fichero = new File("z:/nuevo.txt");
+				try {
+					fichero.createNewFile();
+					JOptionPane.showMessageDialog(null, "Has creado el fichero");
+				} catch (IOException e1) {
+					
+					e1.printStackTrace();
+				}
 				
-				JOptionPane.showMessageDialog(null, " has pulsado el boton");
+				
 			}
 		});
-        
-        // add JLabel to JFrame
-        frame.getContentPane().add(lblText);
-        frame.getContentPane().add(button);
-
-        // display it
-        frame.pack();
-        frame.setVisible(true);
+		
+		frame.getContentPane().add(button);
+		frame.pack();
+		frame.setVisible(true);
 
 	}
 
