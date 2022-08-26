f5d94fd32d6121332b7c50cf1c24ba7c303af6ee
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-26 10:23:46 +0200

    revision scanner

--- java001/src2/com/arquitecturajava/fechas/Principoal2.java
+++ java001/src2/com/arquitecturajava/fechas/Principoal2.java
@@ -7,10 +7,13 @@ public class Principoal2 {
 	public static void main(String[] args) {
 		
 		Scanner sc= new Scanner(System.in);
-		System.out.println("introduce un numero:");
-		int numero=sc.nextInt();
-		
-		System.out.println(numero);
+//		System.out.println("introduce un numero:");
+//		int numero=sc.nextInt();
+//		System.out.println(numero);
+		System.out.println("lee texto");
+		String texto=sc.next();
+		System.out.println(texto);
+		sc.close();
 		
 
 	}
