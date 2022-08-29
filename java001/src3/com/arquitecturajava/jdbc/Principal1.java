e4d3000c739751e5409099c82a12d684c22b8ee5
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-29 10:02:15 +0200

    select  de la base de datos

--- java001/src3/com/arquitecturajava/jdbc/Principal1.java
+++ java001/src3/com/arquitecturajava/jdbc/Principal1.java
@@ -2,7 +2,11 @@ package com.arquitecturajava.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+
 
 public class Principal1 {
 
@@ -17,9 +21,17 @@ public class Principal1 {
 		
 		
 		 try(Connection conn = DriverManager.getConnection(URL, USUARIO, CLAVE);
-		       
-				
+		     Statement sentencia= conn.createStatement(); 
+			 ResultSet rs= sentencia.executeQuery(CONSULTA);
 		      ) {
+			 
+			 while (rs.next()) {
+				 
+				 System.out.println(rs.getString("dni"));
+				 System.out.println(rs.getString("nombre"));
+				 System.out.println(rs.getString("apellidos"));
+				 
+			 }
 			 
 			  System.out.println("estamos conectados");
 		 } catch (SQLException e) {
