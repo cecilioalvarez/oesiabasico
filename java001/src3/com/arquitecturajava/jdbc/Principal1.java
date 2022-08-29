6cb4b4a545605e74980f13017aab0402d1d79cc4
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-29 10:54:31 +0200

    nuevos insercion scanner

--- java001/src3/com/arquitecturajava/jdbc/Principal1.java
+++ java001/src3/com/arquitecturajava/jdbc/Principal1.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Principal1 {
 
@@ -14,20 +15,30 @@ public class Principal1 {
 
 	public static void main(String[] args) {
 
-		String dni="4";
-		String nombre="maria";
-		String apellidos="perez";
 		
-		String consulta = "insert into Personas values ('"+dni+"','" +nombre+"','"+apellidos+"')";
-		try (Connection conn = DriverManager.getConnection(URL, USUARIO, CLAVE);
-				Statement sentencia = conn.createStatement();
+		
+		
+		Scanner sc= new Scanner(System.in);
+		
+		for (int i = 0; i <5; i++) {
+			System.out.println("dame el dni");
+			
+			String dni = sc.next();
+			System.out.println("dame el nombre");
+			String nombre =sc.next();
+			System.out.println("dame el apellido");
+			String apellidos = sc.next();
+			String consulta = "insert into Personas values ('" + dni + "','" + nombre + "','" + apellidos + "')";
+			try (Connection conn = DriverManager.getConnection(URL, USUARIO, CLAVE);
+					Statement sentencia = conn.createStatement();
 
-		) {
-			sentencia.execute(consulta);
-			System.out.println("hemos insertado");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			) {
+				sentencia.execute(consulta);
+				System.out.println("hemos insertado");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
 		}
 
 	}
