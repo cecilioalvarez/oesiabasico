1d218ea36ad0f14f0c0e74840c839288432bb149
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-29 11:44:53 +0200

    cerrar try with resources

---
+++ java001/src3/com/arquitecturajava/jdbc/Principal4.java
@@ -0,0 +1,47 @@
+package com.arquitecturajava.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Principal4 {
+
+	static final String URL = "jdbc:mysql://localhost/curso";
+	static final String USUARIO = "root";
+	static final String CLAVE = "";
+	static final String CONSULTA = "SELECT * FROM Personas where nombre=? and apellidos=?";
+
+	public static void main(String[] args) {
+
+		String nombre = "ana";
+		String apellidos = "sanchez";
+
+		try (Connection conn = DriverManager.getConnection(URL, USUARIO, CLAVE);
+				PreparedStatement sentencia = conn.prepareStatement(CONSULTA);
+
+		) {
+
+			sentencia.setString(1, nombre);
+			sentencia.setString(2, apellidos);
+			try (ResultSet rs = sentencia.executeQuery();) {
+
+				while (rs.next()) {
+
+					System.out.println(rs.getString("nombre"));
+					System.err.println(rs.getString("apellidos"));
+				}
+			} catch (SQLException e) {
+
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
