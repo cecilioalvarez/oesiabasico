cb34b7308c365797d7208e7cc248f49677304d5f
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-08-31 14:56:56 +0200

    nuevos cambios

---
+++ java001/src3/com/arquitecturajava/miproyecto/models/Persona.java
@@ -0,0 +1,107 @@
+package com.arquitecturajava.miproyecto.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Persona {
+
+	
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private int edad;
+	private String pais;
+	private List<Compra> compras = new ArrayList<Compra>();
+
+	public void setCompras(List<Compra> compras) {
+		this.compras = compras;
+	}
+	
+	public List<Compra> getCompras() {
+		return compras;
+	}
+
+	public void addCompra(Compra c) {
+		
+		compras.add(c);
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public Persona(String dni, String nombre, String apellidos, int edad, String pais) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+		this.pais = pais;
+	}
+
+	public Persona() {
+		super();
+	}
+
+	public Persona(String dni) {
+		super();
+		this.dni = dni;
+	}
+
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dni);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(dni, other.dni);
+	}
+
+}
