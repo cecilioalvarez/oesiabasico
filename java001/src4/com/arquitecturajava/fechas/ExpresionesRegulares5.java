37aa29ffe93109f1fdd8e9d7b6159f6c14d669c4
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-09-01 14:14:43 +0200

    patronaje de expresiones regulares

--- java001/src4/com/arquitecturajava/fechas/ExpresionesRegulares5.java
+++ java001/src4/com/arquitecturajava/fechas/ExpresionesRegulares5.java
@@ -7,23 +7,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
-public class ExpresionesRegulares4 {
+public class ExpresionesRegulares5 {
 
 	public static void main(String[] args) {
 
 		String linea = "";
-		Pattern patronImporte = Pattern.compile("\\d{2,5}");
-		Pattern patronNumeroFactura = Pattern.compile("F-?[A-Z\\d]{2,7}");
+		Pattern patron = Pattern.compile("\\d{2,5}");
 		try (FileReader fr = new FileReader("datosregulares.txt"); BufferedReader lector = new BufferedReader(fr);) {
 			while ((linea = lector.readLine()) != null) {
 				
-				Matcher encontrar = patronImporte.matcher(linea);
-				Matcher encontrarNumeroFactura = patronNumeroFactura.matcher(linea);
+				Matcher encontrar = patron.matcher(linea);
 				MatchResult[] lista=encontrar.results().toArray(MatchResult[]::new);
-				MatchResult[] listaFactura=encontrarNumeroFactura.results().toArray(MatchResult[]::new);
-				
 				System.out.println(lista[1].group());
-				System.out.println(listaFactura[0].group());
 				
 			
 			}
