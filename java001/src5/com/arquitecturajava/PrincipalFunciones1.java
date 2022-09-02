5bd8968b741016fed790f36c2c89b75fc85e9e7a
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-09-02 11:56:48 +0200

    funcion statica comparador

--- java001/src5/com/arquitecturajava/PrincipalFunciones1.java
+++ java001/src5/com/arquitecturajava/PrincipalFunciones1.java
@@ -2,36 +2,42 @@ package com.arquitecturajava;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class PrincipalFunciones1 {
 
 	public static void main(String[] args) {
-		
-		
-		
-		List<Persona> bolsaPersonas= new ArrayList<Persona>();
-		bolsaPersonas.add(new Persona("miguel",20));
-		bolsaPersonas.add(new Persona("antonio",30));
-		bolsaPersonas.add(new Persona("ana",10));
-		bolsaPersonas.add(new Persona("jose",15));
-		
-		bolsaPersonas.add(new Persona("david",50));
-		bolsaPersonas.add(new Persona("gema",20));
-		
+
+		List<Persona> bolsaPersonas = new ArrayList<Persona>();
+		bolsaPersonas.add(new Persona("miguel", 20));
+		bolsaPersonas.add(new Persona("antonio", 30));
+		bolsaPersonas.add(new Persona("ana", 10));
+		bolsaPersonas.add(new Persona("jose", 15));
+
+		bolsaPersonas.add(new Persona("david", 50));
+		bolsaPersonas.add(new Persona("gema", 20));
+
 		Collections.sort(bolsaPersonas, new EdadComparator());
 		for (Persona p : bolsaPersonas) {
 
 			System.out.println(p.getNombre());
 		}
 		System.out.println("***********");
-		Collections.sort(bolsaPersonas, new EdadComparator());
+
+		Comparator<Persona> comparador= (o1,o2)->o1.getNombre().compareTo(o2.getNombre());
+		ordenarLista(bolsaPersonas,comparador);
 		
-		Collections.sort(bolsaPersonas, new NombreComparator());
-		for (Persona p : bolsaPersonas) {
+		
+	}
+	public static void ordenarLista(List<Persona> lista, Comparator<Persona> comparador) {
+		
+		Collections.sort(lista,comparador);
+		for (Persona p : lista) {
 
 			System.out.println(p.getNombre());
 		}
+		
 	}
-
+	
 }
