e308b40df1e70f99cc234ca9d442ae694096bfdd
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-09-02 10:22:48 +0200

    nuevas personas

--- java001/src5/com/arquitecturajava/Principal.java
+++ java001/src5/com/arquitecturajava/Principal.java
@@ -1,18 +1,50 @@
 package com.arquitecturajava;
 
+import java.util.Iterator;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		Bolsa b= new Bolsa(5);
-		
+
+		Bolsa<String> b = new Bolsa<String>(5);
+
 		b.add("galletas");
 		b.add("chocolate");
 		b.add("leche");
 		
-		for (String elemento: b) {
-			
+		Bolsa<Persona> bolsaPersonas= new Bolsa<Persona>(5);
+		bolsaPersonas.add(new Persona("miguel"));
+		bolsaPersonas.add(new Persona("antonio"));
+		
+		for (Persona p : bolsaPersonas) {
+
+			System.out.println(p.getNombre());
+		}
+		
+		
+		
+		
+
+		Iterator<String> iterador = b.iterator();
+		while (iterador.hasNext()) {
+
+			System.out.println(iterador.next());
+			iterador.remove();
+		}
+
+		for (String elemento : b) {
+
 			System.out.println(elemento);
 		}
+		
+		
+		
+//		Set conjunto= new HashSet();
+//		
+//		Iterator<String> iterador2= conjunto.iterator();
+//		while (iterador2.hasNext()) {
+//			
+//			System.out.println(iterador2.next());
+//		}
 	}
 }
