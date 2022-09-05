610ffdfac27ba9b3a9bbef59109b07f1b249c6aa
cecilio <cecilio@DESKTOP-RQLPO9J.lan>
2022-09-05 11:13:54 +0200

    Ejemplo 1 de Java Streams

--- java001/src5/com/arquitecturajava/lambdas5/Principal.java
+++ java001/src5/com/arquitecturajava/lambdas5/Principal.java
@@ -1,10 +1,25 @@
 package com.arquitecturajava.lambdas5;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
+		List<Persona> lista= new ArrayList<Persona>();
+		lista.add(new Persona ("pepe","perez",20));
+		lista.add(new Persona ("ana","lopez",30));
+		lista.add(new Persona ("david","gomez",50));
+		lista.add(new Persona ("pedro","perez",10));
+ 		
+		Stream<Persona> flujo= lista.stream();
 		
+		flujo.filter((p)->p.getEdad()>20).forEach((p)-> {
+			
+				System.out.println(p.getNombre());
+		});
 		
 
 	}
